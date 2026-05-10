c7c78787636933d1e6d3bd6a1680edcfa3a2ca4a
Fabian Stenzel <hi@fabianstenzel.com>
2026-05-10 15:45:47 +0200

    experiment 1 added

---
+++ experiment 1/content.js
@@ -0,0 +1,10 @@
+// INSERT A URL OF AN IMAGE
+let replaceURL = "https://www.bu.edu/files/2024/08/Hey-BU-Blog-Headers.jpg";
+
+//GET AN ARRAY OF ALL IMAGES ON A PAGE
+const images = document.getElementsByTagName("img");
+
+//REPLACE THE URL OF THE IMAGES WITH YOUR SPECIFIC URL
+for (image of images) {
+  image.src = replaceURL;
+}
