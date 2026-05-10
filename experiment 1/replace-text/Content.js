956c8a80b00bd2eba141c84d3809ae99d2986d53
Fabian Stenzel <hi@fabianstenzel.com>
2026-05-10 16:25:05 +0200

    experiment 1 two folders

---
+++ experiment 1/replace-text/Content.js
@@ -0,0 +1,24 @@
+// Alle Textelemente auf der Website auswählen
+const elements = document.querySelectorAll("body *");
+
+// Durch alle Elemente gehen
+elements.forEach((element) => {
+  // Prüfen, ob das Element direkten Text enthält
+  if (element.childNodes.length > 0) {
+    element.childNodes.forEach((node) => {
+      // Nur Textknoten verändern
+      if (node.nodeType === Node.TEXT_NODE) {
+        // Leeren Text ignorieren
+        if (node.textContent.trim() !== "") {
+          // Text ersetzen
+          node.textContent = "das ist eine testtext";
+        }
+      }
+    });
+  }
+
+  // OPTIONAL:
+  // Textgröße verändern
+
+  element.style.color = "blue";
+});
