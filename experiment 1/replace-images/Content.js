7cc603f0a4696f8e033378cd00ad8bccffa917e0
Fabian Stenzel <hi@fabianstenzel.com>
2026-05-15 19:41:31 +0200

    Update Content.js

--- experiment 1/replace-images/Content.js
+++ experiment 1/replace-images/Content.js
@@ -2,9 +2,11 @@
 let replaceURL = "https://www.bu.edu/files/2024/08/Hey-BU-Blog-Headers.jpg";
 
 //GET AN ARRAY OF ALL IMAGES ON A PAGE
-let images = document.getElementsByTagName("img");
+let elements = document.getElementsByTagName("img");
+
 
 //REPLACE THE URL OF THE IMAGES WITH YOUR SPECIFIC URL
-for (image of images) {
-  image.src = replaceURL;
-}
+
+elements.forEach((element) => {
+   element.src = replaceURL;
+});
